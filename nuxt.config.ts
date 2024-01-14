0992808e5adb0419b228156dbd03193402208ac5
pavimithra <pavimithra@gmail.com>
2024-01-14 16:22:05 +0400

    CRUD for Columns

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   css: ['~/assets/css/vello.css'],
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt','@nuxt/ui'],
+  modules: ['@pinia/nuxt','@nuxt/ui','@vueuse/nuxt'],
   colorMode: {
     preference: 'light'
   }
